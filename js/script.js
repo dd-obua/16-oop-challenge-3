f36923546220c321b2171ba8a41383c5688255c8
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-15 13:16:33 +0300

    Add chargeBattery to the prototype of Car

--- js/script.js
+++ js/script.js
@@ -6,4 +6,9 @@ const Car = function (make, speed, charge) {
   this.charge = charge;
 };
 
+Car.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+  return this.charge;
+};
+
 const EV = new Car('Tesla', 120, 23);
