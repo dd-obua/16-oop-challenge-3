8e69740db1bca0187eb09c0332ffd0b4feaf4aa0
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-16 12:30:34 +0300

    Create accelerate method on Car.prototype

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
 };
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} going at R{this.speed} km/h`);
+};
