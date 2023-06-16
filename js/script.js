e2169dbf3d10c8e9e8bafb7b0d1610007886faec
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-16 12:10:26 +0300

    Remove EV.prototype.accelerate method

--- js/script.js
+++ js/script.js
@@ -36,16 +36,14 @@ EV.prototype.chargeBattery = function (chargeTo) {
   this.charge = chargeTo;
 };
 
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge--;
-  console.log(
-    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
-  );
-};
-
 const car1 = new EV('Tesla', 120, 23);
 console.log(car1);
 
 car1.chargeBattery(90);
 console.log(car1);
+
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
