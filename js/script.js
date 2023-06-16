d31dee6735939366c97e5e3f0bad00ac85f6fd2e
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-16 12:33:23 +0300

    Fix typo

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ const Car = function (make, speed) {
 
 Car.prototype.accelerate = function () {
   this.speed += 10;
-  console.log(`${this.make} going at R{this.speed} km/h`);
+  console.log(`${this.make} going at ${this.speed} km/h`);
 };
 
 Car.prototype.break = function () {
   this.speed -= 5;
-  console.log(`${this.make} going at R{this.speed} km/h`);
+  console.log(`${this.make} going at ${this.speed} km/h`);
 };
